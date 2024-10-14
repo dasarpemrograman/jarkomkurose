c4846f02ae5abde717e246a2b95a47fc91fe66ec
wisaahmaduta <wiseowisa@gmail.com>
2024-10-14 17:50:08 +0700

    feat added while loop

--- client.py
+++ client.py
@@ -3,26 +3,25 @@ import socket
 # Definisi fungsi untuk client
 def run_client():
     # Definisikan IP dan port server
-    server_ip = "127.0.0.1"
+    server_ip = "192.168.97.183"
     port = 8000
 
     # Buat objek socket UDP
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+
     # Buat pesan untuk dikirim
-    message = "Hello, Server!"
+    message = input()
 
-    try:
-        # Kirim pesan ke server
-        client.sendto(message.encode(), (server_ip, port))
-        print(f"Message sent to {server_ip}:{port}")
+    while message != "exit":
+        try:
+            # Kirim pesan ke server
+            client.sendto(message.encode(), (server_ip, port))
+            print(f"{message} sent to {server_ip}:{port}")
 
-    except Exception as e:
-        print(f"Error: {e}")
-    
-    finally:
-        # Tutup koneksi socket
-        client.close()
+        except Exception as e:
+            print(f"Error: {e}")
+        message = input()
 
 # Jalankan client
 run_client()
